7073a6bedd09184dcbaee9f65177c49c461d1881
Christopher Reece <mrkita.reece@gmail.com>
2018-01-23 08:26:43 +0900

    fixed port variable

--- BookmarkServer.py
+++ BookmarkServer.py
@@ -103,7 +103,9 @@ class Shortener(http.server.BaseHTTPRequestHandler):
             self.send_header('Content-type', 'text/plain; charset=utf-8')
             self.end_headers()
             self.wfile.write('Could not find associated long URI')
+
 if __name__ == '__main__':
-    server_address = ('PORT', 8000)
+    port = int(os.environ.get('PORT', 8000))
+    server_address = ('', port)
     httpd = http.server.HTTPServer(server_address, Shortener)
     httpd.serve_forever()
